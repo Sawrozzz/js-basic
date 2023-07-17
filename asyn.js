7a811fb42bbd2542fa89594719c2708d6609c81d
[saroj adhikari] <[adhikaribroo281@gmail.com]>
2023-07-17 22:14:10 +0545

    my commmit

---
+++ asyn.js
@@ -0,0 +1,9 @@
+
+console.log("First I will Run/ execute");
+
+setTimeout(()=>{
+    console.log("I will run later")  // callback function
+} ,2000)
+
+console.log("I will run secondly")
+    
---
+++ spreadoperator.js
@@ -0,0 +1,41 @@
+// //  let a = [1, 4,5,5];
+// //  let b = [...a];
+// //  console.log(a);
+// //  console.log(b);
+
+// let a = [33, 4, 55, 6]
+// console.log(a)
+// function spread(){
+//     let b=[...a]
+//     console.log(b)
+
+// }
+// spread();
+
+// // concatinating array using SO
+
+// let c =[3,66,77,88]
+// d=[...a, ...c]
+// console.log(d)
+
+//  let obj1 = {
+//     name:"Saroj",
+//     age:22,
+//     faculty:"Scince"
+
+// }
+
+// console.log(obj1);
+
+// let obj2 = {...obj1, language :"js"};
+
+// console.log(obj2);
+
+
+ let abc = {
+    name1: "saroj",
+    age: 22
+ }
+
+ let {name1, age}= abc;  // Destructuring in JS
+ console.log(name1, age);
